ea6d0a8a9aa50130f11454b7f0badc97997ac36a
bietchiminhta-hash <bietchiminhta@gmail.com>
2026-08-12 07:29:34 -0400

    Add CartSlice reducers and AboutUs component

---
+++ src/src/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is dedicated to bringing nature into your home with our
+        carefully curated selection of houseplants. Founded with a passion for
+        greenery and sustainable living,     we source healthy, high-quality plants
+        that thrive indoors and bring life to any space. Our mission is to make
+        plant ownership accessible and enjoyable for everyone, whether you're a
+        seasoned plant parent or just starting your green journey.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
